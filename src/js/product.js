ab479e56a1994c7e49a3e641dd8764a654f389a9
jym126 <jym126@hotmail.com>
2023-01-18 21:41:22 +0100

    following instructions

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,14 @@
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from './ProductDetails.mjs';
 
+const productId = getParam('product');
 const dataSource = new ProductData("tents");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
