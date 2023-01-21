c88788991e8072cb06cce922a03255be563bd705
jym126 <jym126@hotmail.com>
2023-01-22 00:22:25 +0100

    finish

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,3 +1,5 @@
+import {setLocalStorage} from './utils.mjs';
+
 export default class ProductDetails {
   constructor(productId, dataSource) {
     this.productId = productId;
@@ -7,7 +9,9 @@ export default class ProductDetails {
 
   async init() {
     // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+    this.product = await this.dataSource.findProductById(this.productId);
     // once we have the product details we can render out the HTML
+    this.renderProductDetails(this.product);
     // once the HTML is rendered we can add a listener to Add to Cart button
     // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
     document
@@ -15,8 +19,26 @@ export default class ProductDetails {
       .addEventListener("click", this.addToCart.bind(this));
   }
 
-  addToCart() {}
+  addToCart() {
+    setLocalStorage("so-cart", this.product);
+  }
+  // add to cart button event handler
+  // async addToCartHandler(e) {
+  //   const product = await dataSource.findProductById(e.target.dataset.id);
+  //   addProductToCart(product);
+  // }
+  
+  renderProductDetails(product) {
+    document.getElementsByTagName('main')[0].innerHTML = 
+    `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+      <h2 class="divider">${product.NameWithoutBrand}</h2>
+      <img class="divider" src="${product.Image}" alt="${product.NameWithoutBrand}"/>
+      <p class="product-card__price">$${product.FinalPrice}</p>
+      <p class="product__color">${product.Colors[0].ColorName}</p>
+      <p class="product__description">${product.DescriptionHtmlSimple}</p>
+      <div class="product-detail__add">
+        <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+      </div></section>`;
+  }
 
-  renderProductDetails() {}
 }
-
--- src/js/product.js
+++ src/js/product.js
@@ -9,17 +9,3 @@ const product = new ProductDetails(productId, dataSource);
 product.init();
 
 console.log(dataSource.findProductById(productId));
-
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
