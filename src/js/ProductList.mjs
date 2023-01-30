03be849929a1499172e5383e9e5e67397f4214e5
[Miguel] <[leugimmanre@gmail.com]>
2023-01-30 22:53:33 +0100

    Team Activity 4.2

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -29,7 +29,7 @@ async init() {
   // our dataSource will return a Promise...so we can use await to resolve it.
   const list = await this.dataSource.getData();
   // render the list
-  console.log(list);
+  // console.log(list);
   renderListWithTemplate(productTemplate, this.listElement, list);
 }
 }
