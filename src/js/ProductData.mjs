169c46a8b8bb0e04d8c764943018e78451a2aede
jym126 <jym126@hotmail.com>
2023-02-07 00:03:30 +0100

    week 6 assignment

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,3 +1,5 @@
+const baseURL = 'http://server-nodejs.cit.byui.edu:3000/'
+
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -7,17 +9,24 @@ function convertToJson(res) {
 }
 
 export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `../json/${this.category}.json`;
-  }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
-  }
+  // constructor(category) {
+  //   this.category = category;
+  //   this.path = `../json/${this.category}.json`;
+  // }
+  async getData(category) {
+    const response = await fetch(baseURL + `products/search/${category}`);
+    // console.log(response);
+    const data = await convertToJson(response);
+    // console.log(data);
+    return data.Result;
+}
   async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    const response = await fetch(baseURL + `product/${id}`);
+    const data = await convertToJson(response);
+    console.log(data);
+    return data.Result;
+
+    // const products = await this.getData();
+    // return products.find((item) => item.Id === id);
   }
 }
--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -3,9 +3,9 @@ import { renderListWithTemplate } from './utils.mjs';
 function productTemplate(product) {
   return `
       <li class="product-card">
-      <a href="product_pages/index.html?product=${product.Id}">
+      <a href="../product_pages/index.html?product=${product.Id}">
       <img
-          src="${product.Image}"
+          src="${product.Images.PrimarySmall}"
           alt="${product.NameWithoutBrand}"
       />
       <h3 class="card__brand">${product.NameWithoutBrand}</h3>
@@ -26,7 +26,7 @@ export default class ProductList {
 
   async init() {
     // our dataSource will return a Promise...so we can use await to resolve it.
-    const list = await this.dataSource.getData();
+    const list = await this.dataSource.getData(this.category);
     // render the list
     // console.log(list);
     renderListWithTemplate(productTemplate, this.listElement, list);
