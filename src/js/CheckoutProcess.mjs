7e5193b3ca5bbdcb848bcde755f94e73d50b4dc5
Miguel Martínez <leugimmanre@gmail.com>
2023-02-15 23:25:38 +0100

    Team Activity 6

---
+++ src/js/CheckoutProcess.mjs
@@ -0,0 +1,46 @@
+// takes the items currently stored in the cart (localstorage) and returns them in a simplified form.
+function packageItems(items) {
+    // convert the list of products from localStorage to the simpler form required for the checkout process. Array.map would be perfect for this.
+    }
+    
+    export default class CheckoutProcess {
+      ...
+    
+      async checkout(form) {
+        // build the data object from the calculated fields, the items in the cart, and the information entered into the form
+    
+        // call the checkout method in our ExternalServices module and send it our data object.
+      }
+    }
+
+
+export default class CheckoutProcess {
+    constructor(key, outputSelector) {
+      this.key = key;
+      this.outputSelector = outputSelector;
+      this.list = [];
+      this.itemTotal = 0;
+      this.shipping = 0;
+      this.tax = 0;
+      this.orderTotal = 0;
+    }
+    init() {
+      this.list = getLocalStorage(this.key);
+      this.calculateItemSummary();
+    }
+    calculateItemSummary() {
+      // calculate and display the total amount of the items in the cart, and the number of items.
+      
+    }
+    calculateOrdertotal() {
+      // calculate the shipping and tax amounts. Then use them to along with the cart total to figure out the order total
+      
+      // display the totals.
+      this.displayOrderTotals();
+    }
+    displayOrderTotals() {
+      // once the totals are all calculated display them in the order summary page
+      
+    }
+    
+  }
